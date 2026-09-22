d9b04171135a87330ae8f74cea4866788adad33c
pratham1234-cloud <prathamgunjal1234@gmail.com>
2026-09-23 02:29:27 +0530

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return items.reduce((total, item) => {
+
+        const costValue = parsFloat(item.cost.replace('$', ''));
+        return total + costValue * item.quantity;    
+    }, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+   e.preventDefault();
+   onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+       if(item.quantity > 1 ){
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+       }else{
+        dispatch(removeItem(item.name));
+       }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const costValue = parseFloat(item.cost.replace('$', ''));
+    return costValue * item.quantity;
   };
 
   return (
